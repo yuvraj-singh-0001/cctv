8c6bba337b1282b1702619718172eff7efb752d0
yuvraj singh <singhyuvraj8420@gmail.com>
2025-09-30 10:40:45 +0530

    Add search functionality to supplier list and update API response handling

--- server/src/api/master/get-supplier.js
+++ server/src/api/master/get-supplier.js
@@ -3,7 +3,7 @@ const Supplier = require("../../models/supplier");
 const getSuppliers = async (req, res) => {
   try {
     const suppliers = await Supplier.find().sort({ createdAt: -1 });
-    // 👇 is tarah bhejo
+     
     res.json({ suppliers });
   } catch (error) {
     res.status(500).json({ message: "Error fetching suppliers", error: error.message });
